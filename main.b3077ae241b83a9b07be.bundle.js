c3274db549a290cea605983fe059af81ccff3655
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-23 11:39:18 +0000

    Deploy paritytech/substrate-light-ui to github.com/paritytech/substrate-light-ui.git:gh-pages

--- main.b3077ae241b83a9b07be.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{390:function(n,o,p){p(391),p(472),n.exports=p(803)},472:function(n,o,p){"use strict";p.r(o);p(473),p(669),p(796)}},[[390,1,2]]]);
